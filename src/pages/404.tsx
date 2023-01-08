bb80f31be8ef7c01d6ae387c223359466f4d8a56
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-01-08 16:17:21 +0100

    Custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,48 @@
 import * as React from 'react'
 import { Link, HeadFC, PageProps } from 'gatsby'
+import Contact from '../sections/contact'
+import Layout from '../layouts/mainLayout'
 
 const NotFoundPage: React.FC<PageProps> = () => {
+  const [showContact, setShowContact] = React.useState(false)
   return (
-    <main>
-      <h1>Not found</h1>
-    </main>
+    <>
+      <Contact open={showContact} onClose={() => setShowContact(false)} />
+      <Layout
+        openContactForm={() => {
+          setShowContact(true)
+        }}
+      >
+        <main
+          style={{
+            width: '100%',
+            height: '100%',
+            padding: '15rem',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'var(--color-primary-500)',
+            gap: '2rem',
+          }}
+        >
+          <h1 className="text-style-heading-h-1-regular">404</h1>
+          <p className="text-style-heading-h-2-regular">Page not found</p>
+          <Link
+            style={{
+              backgroundColor: 'var(--color-primary-500)',
+              padding: '1rem 2rem',
+              borderRadius: '0.5rem',
+              color: 'var(--color-primary-0)',
+            }}
+            to="/"
+            className="text-style-heading-h-3-regular"
+          >
+            Go back to home
+          </Link>
+        </main>
+      </Layout>
+    </>
   )
 }
 
