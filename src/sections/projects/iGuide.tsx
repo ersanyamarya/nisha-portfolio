08ad50063c0655e8be0d3bcac853878813f7572b
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-01-08 16:45:40 +0100

    Fixed all projects for mobile

--- src/sections/projects/iGuide.tsx
+++ src/sections/projects/iGuide.tsx
@@ -16,9 +16,41 @@ const IGuideProjectPreviewCard = styled(Link)({
   maxHeight: '100vw',
   transform: 'scale(0.95)',
 
-  '&:hover': {
+  '& .graphics': {
+    top: '0%',
+    left: '0%',
+    width: 'fill-available',
+    height: 'fill-available',
+  },
+
+  '& .logo': {
+    transform: 'translate(0%, 0%) scale(1)',
+    top: '4%',
+    left: '15%',
+  },
+
+  '& .description': {
+    transform: 'translate(0%, 0%) scale(1)',
+    top: '23%',
+    left: '10%',
+  },
+
+  '& .mobile': {
     transform: 'scale(1)',
-    boxShadow: 'var(--elevation-focus)',
+    top: '10%',
+    right: '-2%',
+  },
+
+  '& .ipad': {
+    transform: 'scale(1)',
+    top: '50%',
+    right: '12%',
+  },
+
+  '& .desktop': {
+    transform: 'scale(1)',
+    top: '42%',
+    left: '-10%',
   },
 
   '& > .gatsby-image-wrapper': {
@@ -26,81 +58,85 @@ const IGuideProjectPreviewCard = styled(Link)({
     transition: 'var(--transition-ease)',
   },
 
-  '& .graphics': {
-    height: 'fill-available',
-    width: 'fill-available',
-    top: '-15%',
-    left: '-15%',
-  },
-
-  '&:hover > .graphics': {
-    top: '0%',
-    left: '0%',
-  },
-
-  '& .logo': {
-    top: '15%',
-    left: '50%',
-    transform: 'translate(-50%, -50%) scale(1.2)',
-  },
-
-  '&:hover > .logo': {
-    transform: 'translate(0%, 0%) scale(1)',
-    top: '4%',
-    left: '15%',
-  },
-
-  '& .description': {
-    top: '40%',
-    left: '50%',
-    transform: 'translate(-50%, -50%) scale(1.2)',
-  },
-
-  '&:hover > .description': {
-    transform: 'translate(0%, 0%) scale(1)',
-    top: '23%',
-    left: '10%',
-  },
-
-  '& .mobile': {
-    transform: 'scale(0.9)',
-    top: '10%',
-    right: '-10%',
-  },
-
-  '&:hover > .mobile': {
-    transform: 'scale(1)',
-    top: '10%',
-    right: '-2%',
-  },
-
-  '& .ipad': {
-    transform: 'scale(0.9)',
-    top: '50%',
-    right: '10%',
-  },
-
-  '&:hover > .ipad': {
-    transform: 'scale(1)',
-    top: '50%',
-    right: '12%',
-  },
-
-  '& .desktop': {
-    transform: 'scale(0.9)',
-    top: '50%',
-    left: '-15%',
-  },
-
-  '&:hover > .desktop': {
-    transform: 'scale(1)',
-    top: '42%',
-    left: '-10%',
-  },
-
   '@media (max-width: 600px)': {
     '& > .gatsby-image-wrapper': {
       zoom: '50%',
+    },
+  },
+  '@media (min-width: 600px)': {
+    '&:hover': {
+      transform: 'scale(1)',
+      boxShadow: 'var(--elevation-focus)',
+    },
+
+    '& .graphics': {
+      top: '-15%',
+      left: '-15%',
+    },
+
+    '& .logo': {
+      top: '15%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) scale(1.2)',
+    },
+
+    '& .description': {
+      top: '40%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) scale(1.2)',
+    },
+
+    '& .mobile': {
+      transform: 'scale(0.9)',
+      top: '10%',
+      right: '-10%',
+    },
+
+    '& .ipad': {
+      transform: 'scale(0.9)',
+      top: '50%',
+      right: '10%',
+    },
+
+    '& .desktop': {
+      transform: 'scale(0.9)',
+      top: '50%',
+      left: '-15%',
+    },
+
+    '&:hover > .graphics': {
+      top: '0%',
+      left: '0%',
+    },
+
+    '&:hover > .logo': {
+      transform: 'translate(0%, 0%) scale(1)',
+      top: '4%',
+      left: '15%',
+    },
+
+    '&:hover > .description': {
+      transform: 'translate(0%, 0%) scale(1)',
+      top: '23%',
+      left: '10%',
+    },
+
+    '&:hover > .mobile': {
+      transform: 'scale(1)',
+      top: '10%',
+      right: '-2%',
+    },
+
+    '&:hover > .ipad': {
+      transform: 'scale(1)',
+      top: '50%',
+      right: '12%',
+    },
+
+    '&:hover > .desktop': {
+      transform: 'scale(1)',
+      top: '42%',
+      left: '-10%',
     },
   },
 })
