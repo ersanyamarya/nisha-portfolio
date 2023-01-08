a6b29ab46159f14e899a92fc58349d2d70f28381
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-01-08 16:45:56 +0100

    fixed all contents for mobile

--- src/sections/projects/pawsAndClaws.tsx
+++ src/sections/projects/pawsAndClaws.tsx
@@ -16,9 +16,35 @@ const PawsProjectPreviewCard = styled(Link)({
   maxHeight: '100vw',
   transform: 'scale(0.95)',
 
-  '&:hover': {
+  '& .graphics': {
+    height: 'fill-available',
+    width: 'fill-available',
+    top: '0%',
+    left: '0%',
+  },
+
+  '& .logo': {
+    transform: 'translate(0%, 0%) scale(1)',
+    top: '15%',
+    left: '10%',
+  },
+
+  '& .description': {
+    transform: 'translate(0%, 0%) scale(1)',
+    top: '33%',
+    left: '10%',
+  },
+
+  '& .mobile': {
     transform: 'scale(1)',
-    boxShadow: 'var(--elevation-focus)',
+    top: '-4%',
+    right: '-2%',
+  },
+
+  '& .desktop': {
+    transform: 'scale(1)',
+    top: '60%',
+    left: '15%',
   },
 
   '& > .gatsby-image-wrapper': {
@@ -26,68 +52,74 @@ const PawsProjectPreviewCard = styled(Link)({
     transition: 'var(--transition-ease)',
   },
 
-  '& .graphics': {
-    height: 'fill-available',
-    width: 'fill-available',
-    top: '-15%',
-    left: '-15%',
-  },
-
-  '&:hover > .graphics': {
-    top: '0%',
-    left: '0%',
-  },
-
-  '& .logo': {
-    top: '18%',
-    left: '50%',
-    transform: 'translate(-50%, -50%) scale(1.2)',
-  },
-
-  '&:hover > .logo': {
-    transform: 'translate(0%, 0%) scale(1)',
-    top: '15%',
-    left: '10%',
-  },
-
-  '& .description': {
-    top: '40%',
-    left: '50%',
-    transform: 'translate(-50%, -50%) scale(1.2)',
-  },
-
-  '&:hover > .description': {
-    transform: 'translate(0%, 0%) scale(1)',
-    top: '33%',
-    left: '10%',
-  },
-
-  '& .mobile': {
-    transform: 'scale(0.9)',
-    top: '10%',
-    right: '-10%',
-  },
-
-  '&:hover > .mobile': {
-    transform: 'scale(1)',
-    top: '-4%',
-    right: '-2%',
-  },
-
-  '& .desktop': {
-    transform: 'scale(0.9)',
-    top: '38%',
-    left: '25%',
-  },
-
-  '&:hover > .desktop': {
-    transform: 'scale(1)',
-    top: '60%',
-    left: '15%',
-  },
   '@media (max-width: 600px)': {
     '& > .gatsby-image-wrapper': {
       zoom: '50%',
+    },
+  },
+
+  '@media (min-width: 600px)': {
+    '&:hover': {
+      transform: 'scale(1)',
+      boxShadow: 'var(--elevation-focus)',
+    },
+
+    '& .graphics': {
+      top: '-15%',
+      left: '-15%',
+    },
+
+    '& .logo': {
+      top: '18%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) scale(1.2)',
+    },
+
+    '& .description': {
+      top: '40%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) scale(1.2)',
+    },
+
+    '& .mobile': {
+      transform: 'scale(0.9)',
+      top: '10%',
+      right: '-10%',
+    },
+
+    '& .desktop': {
+      transform: 'scale(0.9)',
+      top: '38%',
+      left: '25%',
+    },
+
+    '&:hover > .graphics': {
+      top: '0%',
+      left: '0%',
+    },
+
+    '&:hover > .logo': {
+      transform: 'translate(0%, 0%) scale(1)',
+      top: '15%',
+      left: '10%',
+    },
+
+    '&:hover > .description': {
+      transform: 'translate(0%, 0%) scale(1)',
+      top: '33%',
+      left: '10%',
+    },
+
+    '&:hover > .mobile': {
+      transform: 'scale(1)',
+      top: '-4%',
+      right: '-2%',
+    },
+
+    '&:hover > .desktop': {
+      transform: 'scale(1)',
+      top: '60%',
+      left: '15%',
     },
   },
 })
