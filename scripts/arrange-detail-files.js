e08d8667763a80daf2eb899f366d71b6688e4289
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-08-29 01:03:54 +0200

    Fix images

--- scripts/arrange-detail-files.js
+++ scripts/arrange-detail-files.js
@@ -3,9 +3,11 @@ const { readdirSync, renameSync } = require('fs')
 const DETAILS_DIR = join(__dirname, '../src/images/details')
 
 const projects = readdirSync(DETAILS_DIR)
+const twoDigit = num => (num < 10 ? `0${num}` : num)
 
 console.log(projects)
 projects.forEach(cleanProject)
+// cleanProject(projects[5])
 
 function cleanProject(project) {
   if (project === '.DS_Store') return
@@ -14,7 +16,7 @@ function cleanProject(project) {
   // rename all files to be lowercase and replace spaces with dashes and replace file on disk
   files.forEach((file, index) => {
     const oldPath = join(projectPath, file)
-    const newPath = join(projectPath, `${index}-page.png`)
+    const newPath = join(projectPath, `${twoDigit(index + 1)}-page.png`)
     renameSync(oldPath, newPath)
   })
 }
