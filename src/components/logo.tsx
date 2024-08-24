073e260379b9c5e4c2f8d846a9c3caf7e5987f3b
--global <-g>
2024-08-24 10:55:01 +0200

    chore: Refactor logo component and mainLayout navBar styles

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,9 +8,8 @@ export function Logo({ ...props }) {
       to="/">
       {/* <StaticImage width={64} src="../../images/icon.png" alt="REPLACE WITH ACTUAL COPY" /> */}
       <div className="gap-0.1 flex text-3xl font-medium">
-        <span className="p-1 text-primary">N</span>
-
-        <span className="rounded-sm bg-primary p-1 text-primary-50">K</span>
+        <span className="px-1 py-0.5 text-primary">N</span>
+        <span className="rounded-sm bg-primary px-1 py-0.5 text-primary-50">K</span>
       </div>
     </Link>
   );
